2d3f86e5a7a3bc4182d108bf01ad549228082fe9
souhub <63490152+souhub@users.noreply.github.com>
2021-09-21 00:38:29 +0900

    feat: add dmmapi

--- init-actresses/app.py
+++ init-actresses/app.py
@@ -1,7 +1,15 @@
 import json
 import traceback
+import os
+
+import requests
 
 from model.algorithms import decide_init_actress_id
+
+
+API_ID = os.environ.get('API_ID')
+AFFIRIATE_ID = os.environ.get('AFFIRIATE_ID')
+DMM_API_BASE_URL = os.environ.get('DMM_API_BASE_URL')
 
 
 def lambda_handler(event, context):
@@ -11,10 +19,34 @@ def lambda_handler(event, context):
         ids = decide_init_actress_id()
         # ここまで
 
+        actresses = []
+
+        for id in ids:
+            payload = {'api_id': API_ID,
+                       'affiliate_id': AFFIRIATE_ID,
+                       'actress_id': id}
+
+            r = requests.get(
+                DMM_API_BASE_URL, params=payload)
+
+            actress = r.json()['result']['actress'][0]
+
+            try:
+                # imageURLがない女優は消去
+                actress['imageURL']
+                actresses.append(actress)
+            except:
+                continue
+
+            for k in actress:
+                if actress[k] == "" or actress[k] is None:
+                    actress[k] = '♡♡秘密♡♡'
+
         return {
             'statusCode': 200,
-            'body': json.dumps({"ids": ids})
+            'body': json.dumps({"actresses": actresses})
         }
+
     except Exception as e:
         print(e)
         return {
