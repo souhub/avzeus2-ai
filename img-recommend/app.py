7d1ec347adcd5fc914ed5870e1016601ae8b3bbe
souhub <63490152+souhub@users.noreply.github.com>
2021-11-09 12:52:16 +0900

    update

--- img-recommend/app.py
+++ img-recommend/app.py
@@ -1,12 +1,23 @@
 import uuid
 import json
 import traceback
+import os
 
 import boto3
-
+import requests
 from model.algorithms import recommend_from_img_path
 
+API_ID = os.environ.get('API_ID')
+AFFIRIATE_ID = os.environ.get('AFFIRIATE_ID')
+DMM_API_BASE_URL = os.environ.get('DMM_API_BASE_URL')
+
 s3_client = boto3.client('s3')
+
+headers = {
+    'Access-Control-Allow-Headers': 'Content-Type, X-Requested-With',
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'OPTIONS, POST',
+}
 
 
 def lambda_handler(event, context):
@@ -14,17 +25,18 @@ def lambda_handler(event, context):
 
     unique_key = '{}.jpeg'.format(uuid.uuid4())
     download_path = '/tmp/{}'.format(unique_key)
+    actresses = []
 
     # .jpeg 以外の拡張子だとエラーが出るため
     try:
         is_face, ids = recommend_from_img_path(body, download_path)
-
         if not is_face:
             return {
                 'statusCode': 200,
+                'headers': headers,
                 'body': json.dumps({
-                    "is_face": is_face,
-                    "ids": ids
+                    "actresses": actresses,
+                    "key": ''
                 })
             }
 
@@ -40,11 +52,23 @@ def lambda_handler(event, context):
             Key=unique_key,
             ContentType=content_type)
 
+        # ID配列から女優情報を取得
+
+        for id in ids:
+            payload = {'api_id': API_ID,
+                       'affiliate_id': AFFIRIATE_ID,
+                       'actress_id': id}
+
+            r = requests.get(
+                DMM_API_BASE_URL, params=payload)
+
+            actress = r.json()['result']['actress'][0]
+            actresses.append(actress)
         return {
             'statusCode': 200,
+            'headers': headers,
             'body': json.dumps({
-                "is_face": is_face,
-                "ids": ids,
+                "actresses": actresses,
                 "key": 'https://dpgtkhmgfsuru.cloudfront.net/{}'.format(unique_key)
             })
         }
@@ -52,14 +76,6 @@ def lambda_handler(event, context):
         print(e)
         return {
             'statusCode': 500,
+            'headers': headers,
             'body': json.dumps({"error": traceback.format_exc()})
-        }
-
-    except:
-        return {
-            'statusCode': 200,
-            'body': json.dumps({
-                "is_face": is_face,
-                "ids": ids
-            })
         }
